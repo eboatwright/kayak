5a3b458feeda9b86bdee842d72afac31a20bac91
eboatwright <eboatwright7236@gmail.com>
2022-02-02 13:51:03 -0600

    2/2/22 1:51 PM
     - added basics.rs in examples
     - added empty files
     - added State
     - added EmptyState
     - added ResourceContainer
     - added NoResources
     - added Master
     - added convert_resources
     - added start

---
+++ examples/basics.rs
@@ -0,0 +1,66 @@
+use macroquad::prelude::*;
+use rutoe::*;
+
+// See Macroquad's docs.rs page for more on rendering and window configuration
+// NOTE: Some things will be different, as this project uses my modified version of macroquad.
+// https://github.com/eboatwright/macroquad/
+// https://docs.rs/macroquad/0.3.13/macroquad/
+#[macroquad::main("Hello RUTOE!")]
+async fn main() {
+	let mut master = Master {
+		state: Box::new(GameState {
+			// Just put empty values here because initialize will get called on it
+			player_score: 0,
+		}),
+		resources: Box::new(Resources {
+			// Load all your textures, sounds and fonts here
+			// example_texture: load_texture("res/img/this_texture_doesnt_exist.png").await.unwrap(),
+		}),
+		// Or if your game doesn't have any textures, sounds or fonts just do
+		// ..Default::default()
+	};
+
+	start(&mut master).await;
+}
+
+// Put all your game variables here
+struct GameState {
+	player_score: u16,
+}
+
+// Implement all state functions
+impl State for GameState {
+	fn initialize(&mut self) {
+		// Will be called when the state is loaded
+		println!("GameState Initialize!");
+	}
+
+	fn update(&mut self) {
+		// Will be called every frame (only on the current scene in Master)
+		println!("GameState Update!");
+	}
+
+	fn render(&self, _resources: &Box<dyn ResourceContainer>) {
+		// Will be called every frame after update (only on the current scene in Master)
+		// This converts the Box trait into your Resources struct
+		// Replace this with your Resources struct's name
+		//                     |
+		// let resources: &Resources = convert_resources(resources);
+		println!("GameState Render!");
+		draw_text(
+			&format!("Player Score: {}", self.player_score),
+			20.0,
+			20.0,
+			16.0,
+			WHITE,
+		);
+	}
+}
+
+// Put all your textures, sounds and fonts in here
+struct Resources {
+	// example_texture: Texture2D,
+}
+
+// This is just a marker trait
+impl ResourceContainer for Resources {}
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use macroquad::prelude::*;
+
+pub trait State {
+	fn initialize(&mut self);
+	fn update(&mut self);
+	fn render(&self, resources: &Box<dyn ResourceContainer>);
+}
+
+struct EmptyState {}
+impl State for EmptyState {
+	fn initialize(&mut self) {}
+	fn update(&mut self) {}
+	fn render(&self, _resources: &Box<(dyn ResourceContainer)>) {}
+}
+
+pub trait ResourceContainer {}
+
+struct NoResources {}
+impl ResourceContainer for NoResources {}
+
+pub struct Master {
+	pub state: Box<dyn State>,
+	pub resources: Box<dyn ResourceContainer>,
+}
+
+impl Default for Master {
+	fn default() -> Self {
+		Self {
+			state: Box::new(EmptyState {}),
+			resources: Box::new(NoResources {}),
+		}
+	}
+}
+
+pub fn convert_resources<T: 'static + ResourceContainer>(resources: &Box<dyn ResourceContainer>) -> &T {
+	(resources as &dyn std::any::Any).downcast_ref::<T>().unwrap()
+}
+
+pub async fn start(master: &mut Master) {
+	master.state.initialize();
+	loop {
+		master.state.update();
+
+		master.state.render(&master.resources);
+
+		next_frame().await
+	}
+}
