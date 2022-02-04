e198a97cf2ae7ba199de3d99b3f4a13f762e7f52
eboatwright <eboatwright7236@gmail.com>
2022-02-03 21:04:39 -0600

    2/3/22 9:04 PM
     - built flappy bird example to wasm
     - updated macroquad version

--- examples/flappy_bird/web/build_wasm.sh
+++ examples/flappy_bird/web/build_wasm.sh
@@ -1,3 +1,3 @@
 rustup target add wasm32-unknown-unknown
 cargo build --release --target=wasm32-unknown-unknown
-cp ../target/wasm32-unknown-unknown/release/flappy_bird.wasm web
+cp ../target/wasm32-unknown-unknown/release/flappy_bird.wasm flappy_bird.wasm
